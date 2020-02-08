7945b8f34c9ac82e33cd4b2dc12887dc21a8ccd2
Di Zheng <dizhengcindy@gmail.com>
2020-02-08 17:18:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,16 +1,27 @@
 def using_push(array, string)
   array = array.push(string)
 end
+
 def using_unshift(array,string)
   array = array.unshift(string)
 end
+
 def using_pop(array)
   array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
 end
 def shift_with_args(array)
   array.shift(2)
 
 end
+
 def using_concat(array1,array2)
   array1 = array1.concat(array2)
 end
@@ -29,4 +40,5 @@ def using_delete(array,string)
   array.delete(string)
 end
 def using_delete_at(array,int)
+  array.delete(int)
 end
